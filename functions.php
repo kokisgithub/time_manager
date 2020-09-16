f760d47eacbd423b08a3159716570fb421e7038a
kokisgithub <kmagastbsb.nvng816@gmail.com>
2020-09-16 13:08:16 +0900

    htmlspecialchars導入
    htmlspecialcharsを導入したため

--- functions.php
+++ functions.php
@@ -1,21 +1,6 @@
-  <?php 
+<?php
 
-  $time1 = filter_input(INPUT_POST, 'time1');
-  $time2 = filter_input(INPUT_POST, 'time2');
+function h($str){
 
-  $t1 = new DateTime($time1);
-  $t2 = new DateTime($time2);
-
-  $interval = $t1->diff($t2);
-
-  $interval = $interval->format('%H:%I');
-
-  function interval($interval)
-  {  
-    if ($interval !== '00:00' || null){
-      echo $interval;
-     }
-  }
-  
-
-  ?>
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
